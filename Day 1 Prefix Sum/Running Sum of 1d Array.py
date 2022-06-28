b17d5fb99bf9bdf50420f1e69722c0beb677a138
Burhanuddin <b.plumber@somaiya.edu>
2022-06-28 17:53:09 +0530

    1480

---
+++ Day 1 Prefix Sum/Running Sum of 1d Array.py
@@ -0,0 +1,7 @@
+class Solution:
+    def runningSum(self, nums: List[int]) -> List[int]:
+        sm = 0
+        for i in range(len(nums)):
+            sm+=nums[i]
+            nums[i] = sm
+        return nums
